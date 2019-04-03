50ad9c04fe6e366885a4f0dd8f68bae5c0d38b3e
YuriiAndrukhiv <48164647+YuriiAndrukhiv@users.noreply.github.com>
2019-04-03 23:45:22 +0300

    Updated main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
-
+#include "Employee.h"
+#include "Office.h"
 #include "printable.h"
 
 /*
@@ -27,39 +28,16 @@ void printList(const std::vector<Printable *> listToPrint)
 
 int main()
 {
-    // ЗАВДАННЯ:
-    // ---------
-    // 1. Створіть новий клас Employee (працівник) та додайте до нього методи
-    // для задання різноманітної інформації про працівника:
-    //    * Ім'я
-    //    * Номер телефону
-    //    * Домашня адреса
-    //    * Заробітна плата (число у доларах США)
-    //    * З якого року працює
-    // Клас має мати окремий *.h-файл з описом та окремий *.cpp файл що міститиме реалізацію.
-    // 2. Для того, щоб вивести інформацію про працівника, успадкуйте його від класу Printable
-    // та перевизначіть метод print(). У цьому методі виведіть усю доступну про працівника
-    // інформацію у консоль.
-    // 3. Створіть 2-3 об'єкта класу Employee, додайте їх до списку employeesToPrint
-    // та виведіть з допомогою функції printList().
-    //
-    // 4. Створіть новий клас Office (офіс) який буде містити інформацію про офіси
-    // нашої фірми та їх місцезнаходження. Додайте до нього методи для задання
-    // різноманітної інформації:
-    //    * Назва офісу
-    //    * Адреса офісу
-    //    * Робоча площа (у кв метрах)
-    //    * Кількість працівників що працюють всередині
-    // Клас має мати окремий *.h-файл з описом та окремий *.cpp файл що міститиме реалізацію.
-    // 5. Створіть 2-3 об'єкта класу Office, додайте їх до списку officesToPrint
-    // та виведіть з допомогою функції printList().
-    // Для того, щоб вивести інформацію про офіс, успадкуйте його від класу Printable
-    // та перевизначіть метод print(). У цьому методі виведіть усю доступну про офіс
-    // інформацію у консоль.
 
     std::vector<Printable *> employeesToPrint;
 
-    // Створіть об'єкти Employee та наповніть список тут....
+    Printable *employee1 = new Employee("Semen", "+380634562321", "Brodivska 1", 8000, 2000);
+    Printable *employee2 = new Employee("Oleksa", "+380988563796", "Tuha voda 7", 8500, 1998);
+    Printable *employee3 = new Employee("Misko", "+380638887318", "Sheroka 66", 9000, 1995);
+    employeesToPrint.push_back(employee1);
+    employeesToPrint.push_back(employee2);
+    employeesToPrint.push_back(employee3);
+
 
     printList(employeesToPrint);
 
@@ -67,9 +45,22 @@ int main()
 
     std::vector<Printable *> officesToPrint;
 
-    // Створіть об'єкти Office та наповніть список тут....
+    Printable *office1 = new Office("Head office", "Plosha rinok 1", 320, 100);
+    Printable *office2 = new Office("Branch1", "Mazepi 3", 165, 50);
+    Printable *office3 = new Office("Branch2", "Virmenska 7", 100, 30);
+    officesToPrint.push_back(office1);
+    officesToPrint.push_back(office2);
+    officesToPrint.push_back(office3);
 
     printList(officesToPrint);
+
+    delete office3;
+    delete office2;
+    delete office1;
+
+    delete employee3;
+    delete employee2;
+    delete employee1;
 
     return 0;
 }
